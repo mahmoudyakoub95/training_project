46648bf0f36ae32b157fc743d65c1e7acc3ad476
mahmoudyakoub95 <mahmoud.yakoub95@gmail.comuser.emai>
2026-08-13 12:51:34 +0200

    add grade checker

--- main.py
+++ main.py
@@ -2,4 +2,12 @@ def greet():
     return "Hello from main"
 
 
+def grade_result(score):
+    if score >= 50:
+        return "pass"
+    else:
+        return "fail"
+
+
 print(greet())
+print(grade_result(80))
